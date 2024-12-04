ed2c2d7b6525bb185d2e009f5717b644948b1ce1
Arnab Paul <you@example.com>
2024-12-04 22:42:41 +0530

    frontend sorted

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "@/Layout/MainLayout";
-import HomePage from "@/pages/Home/Home";
+import HomePage from "./pages/Home/Home";
 import LoginForm from "./pages/Login/Login";
 import SignUpForm from "./pages/SignUp/SignUp";
 
-// import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
-// import RootWrapper from "./Layout/RootWrapper";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import RootWrapper from "./Layout/RootWrapper";
 // import VerificationEmailSent from "./pages/EmailVerification/VerificationEmailSent";
 // import VerificationStatus from "./pages/EmailVerification/VerificationStatus";
 // import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
@@ -25,14 +25,14 @@ const router = createBrowserRouter([
     element: <RootWrapper />,
     children: [
       {
-        path: "/",
-        element: <ProtectedRoute />,
-        children: [
-          {
-            path: "/Features",
-            element: <Features />,
-          },
-        ],
+        // path: "/",
+        // element: <ProtectedRoute />,
+        // children: [
+        //   {
+        //     path: "/Features",
+        //     element: <Features />,
+        //   },
+        // ],
       },
       {
         path: "/",
@@ -47,22 +47,22 @@ const router = createBrowserRouter([
         path: "/SignUp",
         element: <SignUpForm />,
       },
-      {
-        path: "/verifymail",
-        element: <VerificationEmailSent />,
-      },
-      {
-        path: "/verifymail/:verificationToken",
-        element: <VerificationStatus />,
-      },
-      {
-        path: "/forgot-password",
-        element: <ForgotPassword />,
-      },
-      {
-        path: "/reset-password/:resetToken",
-        element: <PasswordResetForm />,
-      },
+      // {
+      //   path: "/verifymail",
+      //   element: <VerificationEmailSent />,
+      // },
+      // {
+      //   path: "/verifymail/:verificationToken",
+      //   element: <VerificationStatus />,
+      // },
+      // {
+      //   path: "/forgot-password",
+      //   element: <ForgotPassword />,
+      // },
+      // {
+      //   path: "/reset-password/:resetToken",
+      //   element: <PasswordResetForm />,
+      // },
     ],
   },
 ]);
